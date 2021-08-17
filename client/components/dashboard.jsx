c6a132cd5eab12ebcdd609a7966fb77a00384dfb
Kavyashreekn-2000 <kavshre@gmail.com>
2021-08-17 21:37:51 +0530

    4done

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -4,23 +4,47 @@ export default class dash extends Component {
     render() {
         return (
             <form>
-                <div class="form-box">
+               
+                  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">DASHBOARD</span>
+    </div>
+      <ul class="nav-links">
+      <li>
+          <a href="/profile">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="links_name">View profile</span>
+          </a>
+        </li>
 
-                
-                <div class="header-text">
-			    LOGIN FORM
-		        </div>
+        
+        <li>
+          <a href="#">
+            <i class='bx bx-box' ></i>
+            <span class="links_name">Add crop</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-list-ul' ></i>
+            <span class="links_name">Orders</span>
+          </a>
+        </li>
+        
+         
+        <li class="log_out">
+          <a href="#">
+            <i class='bx bx-log-out'></i>
+            <span class="links_name">Log out</span>
+          </a>
+        </li>
+      </ul>
+  </div>
+    
+  
 
-                
-        <input placeholder="Enter Email Address" type="text"/>
-        <input placeholder="Enter Password" type="password"/> 
-        <input id="terms" type="checkbox"/>
-        <button>login</button>
-        Don't have an account? <a href="/signup">Register Here</a>
-	</div>
-            </form>
-
-
+  </form>
 
         );
     }
