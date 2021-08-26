f0427843fbf650a2ee42d271629b8609b3fd8919
Chanakya <ucs3721@outlook.com>
2021-08-26 11:08:40 +0530

    update api project sturcture

---
+++ api/controllers/user.js
@@ -0,0 +1,32 @@
+/// <reference types="express"/>
+
+import { Router } from "express";
+import profiles from "../data/users.js";
+const router = Router();
+
+router.get("/", (req, res) => {
+	res.json(profiles);
+});
+
+//Post Methods
+router.post("/", (req, res) => {
+	const profile = {
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		mobile: req.body.mobile,
+		city: req.body.city,
+	};
+	profiles.push(profile);
+	res.json(profile);
+});
+
+// route for get api is /api/profile/id
+router.get("/:id", (req, res) => {
+	let a = profiles.find((c) => c.firstName === req.params.id);
+	res.json(a);
+	console.log(req.query);
+	console.log(typeof req.params.id);
+});
+
+export default router;
---
+++ api/data/users.js
@@ -0,0 +1,18 @@
+const profiles = [
+  {
+    firstName: "badman",
+    lastName: "node",
+    email: "harsh@gmail.com",
+    mobile: "7207737150",
+    city: "Mysore",
+  },
+  {
+    firstName: "justman",
+    lastName: "deno",
+    email: "okok@gmail.com",
+    mobile: "8179229558",
+    city: "Denver",
+  },
+];
+
+export default profiles;
