197cfa87b43d8b49633376202523b4d3e0ec3a56
Harshith2001 <harshith1450@gmail.com>
2021-08-30 13:25:59 +0530

    Added `Database.js`

---
+++ api/util/database.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+class database {
+	constructor(fileName) {
+		this.fileName = fileName;
+	}
+	read() {
+		return fs.readFileSync(this.fileName, "utf-8");
+	}
+	write(data) {
+		fs.writeFileSync(this.fileName, JSON.stringify(data));
+	}
+}
