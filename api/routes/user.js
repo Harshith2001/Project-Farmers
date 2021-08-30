43ceeea0981c55458a1337ed3ab91562a816d969
Harshith2001 <harshith1450@gmail.com>
2021-08-30 17:03:09 +0530

    profile api phase 1

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,38 +2,35 @@
 
 import { Router } from "express";
 import database from "../util/database.js";
+
 // Routes - "/api/user/"
 const router = Router();
 const profileDb = new database("./databases/profile.json");
-
+const dbData = profileDb.read();
 router.get("/", (req, res) => {
-	let x = profileDb.read();
-	res.json(x.data);
+	res.json(dbData.data);
 });
 
 //Post Methods
 router.post("/", (req, res) => {
 	const profile = {
-		userId: req.body.id,
+		userId: req.body.userId,
 		userType: req.body.userType,
 		name: req.body.name,
 		email: req.body.email,
 		mobile: req.body.mobile,
 		city: req.body.city,
 	};
-	//console.log("post req got: " + req.url);
-	let oldData = JSON.parse(profileDb.read());
-	let newData = oldData.data.push(profile);
-	profileDb.write(newData);
+	dbData.data.push(profile);
+	// console.log(oldData);
+	profileDb.write(dbData);
 	res.status(201).json(profileDb);
 });
 
 // route for get api is /api/profile/id
 router.get("/:id", (req, res) => {
-	let a = profileDb.find((user) => user.id === req.params.id);
+	let a = dbData.data.find((user) => user.userId === req.params.id);
 	res.json(a);
-	console.log(req.query);
-	console.log(typeof req.params.id);
 });
 
 export default router;
