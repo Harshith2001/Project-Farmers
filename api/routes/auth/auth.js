ec91d0a1d2c8496bee056f3271f76b99edba1680
Chanakya <ucs3721@outlook.com>
2021-09-02 12:27:34 +0530

    probably working auth API

--- api/routes/auth/auth.js
+++ api/routes/auth/auth.js
@@ -1,43 +1,72 @@
 import { Router } from "express";
+import database from "../util/database.js";
 
 // Route - "/auth/"
 const router = Router();
 
+const userDb = new database("./databases/profile.json");
+const authDb = new database("./databases/auth.json");
+let userData = userDb.read();
+let authData = authDb.read();
+
+// Route - "/auth/login"
 router.get("/login", (req, res) => {
 	// get body which has userId and password
 	const { userId, password } = req.body;
 
-	// check if userId and password are valid
-	if (userId === "admin" && password === "admin") {
-		// if valid, set session
-		req.session.userId = userId;
+	// check if userId is in database
+	if (authData.data.find((users) => users.userId === userId)) {
+		// check if password is correct
+		if (authData.data.find((users) => users.userId === userId).password === password) {
+			// if correct, send userId
+			req.session.userId = userId;
+			res.json({
+				success: true,
+				userId: userId,
+			});
+		} else {
+			// if incorrect, send error
+			res.send({
+				success: false,
+				message: "Incorrect password",
+			});
+		}
+	} else {
+		// if userId is not in database, send error
 		res.json({
-			success: true,
-			message: "login success",
+			success: false,
+			message: "UserId not found",
 		});
 	}
+});
 
-	// if not valid, return error
+// Route - "/auth/signup"
+// sign up the user
+router.post("/signup", (req, res) => {
+	var auth_user = {
+		userId: req.body.userId,
+		password: req.body.password,
+	};
+	var newUser = {
+		name: req.body.name,
+		userId: req.body.id,
+		userType: req.body.userType,
+		dateCreated: new Date(),
+	};
+	authData.data.push(auth_user);
+	userData.data.push(newUser);
+
+	authDb.write(authData);
+	userDb.write(userData);
+
+	req.session.userId = auth_user.userId;
 	res.json({
-		success: false,
-		message: "login failed",
+		success: true,
+		userId: auth_user.userId,
 	});
 });
 
 // Route - "/auth/logout"
-// sign up the user
-router.post("/signup", (req, res) => {
-	if (!req.body.id || !req.body.password) {
-		res.status("400");
-		res.send("Invalid details!");
-	} else {
-		var newUser = { id: req.body.id, password: req.body.password };
-		Users.push(newUser);
-		req.session.user = newUser;
-		res.redirect("/protected_page");
-	}
-});
-
 // logout
 router.get("/logout", (req, res) => {
 	// delete session
