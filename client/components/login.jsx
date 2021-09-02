247545d5341a24da31596ee5b005ef5669a0fe98
Chanakya <ucs3721@outlook.com>
2021-09-02 16:37:46 +0530

    Working API calling from client 🥳 - Tested only login

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,21 +1,56 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.formSubmit = this.formSubmit.bind(this);
+		this.userId = React.createRef();
+		this.password = React.createRef();
+	}
+
+	formSubmit() {
+		let req = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				userId: this.userId.current.value,
+				password: this.password.current.value,
+			}),
+		};
+		fetch(process.env.API_URL + "/auth/login", req)
+			.then((x) => x.json())
+			.then((x) => {
+				console.log(x);
+				if (x.success) {
+					localStorage.setItem("userId", x.userId);
+				} else {
+					alert(x.message);
+				}
+			})
+			.catch((err) => {
+				alert(err);
+			});
+	}
+
 	render() {
+		//console.log(process.env.API_URL);
+		fetch(process.env.API_URL + "/api/user")
+			.then((x) => x.json())
+			.then((data) => console.log(data));
 		return (
-			<form>
-				<div className="form-box">
-					<div className="header-text">LOGIN FOR </div>
+			<div className="form-box">
+				<div className="header-text">LOGIN FOR </div>
 
-					<input placeholder="Enter Email Address" type="text" />
-					<input placeholder="Enter Password" type="password" />
+				<input ref={this.userId} placeholder="Enter your User ID" type="text" />
+				<input ref={this.password} placeholder="Enter Password" type="password" />
 
-					<button>login</button>
-					<div className="align">
-						Don't have an account? <a href="/signup">Register Here</a>
-					</div>
+				<button onClick={this.formSubmit}>login</button>
+				<div className="align">
+					Don't have an account? <a href="/signup">Register Here</a>
 				</div>
-			</form>
+			</div>
 		);
 	}
 }
--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	env: {
+		API_URL: "http://localhost:3100",
+	},
+};
