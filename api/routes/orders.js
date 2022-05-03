605bc2911c810971fea312432c144308351c3639
Harshith2001 <harshith1450@gmail.com>
2022-05-04 00:13:33 +0530

    orders.js buggy version

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,44 +1,37 @@
 import { Router } from "express";
 import productModel from "../models/productModel.js";
 import orderModel from "../models/orderModel.js";
+import { ObjectId } from "mongodb";
 
 const router = Router();
+const objectId = ObjectId;
 // Irrespective of user type this api will return orders made by the user for the faster data retrievel to filed such as fUserId and eUserId are used.
 // fuserid or euserid or order id is used to retrieve the orders.
 router.get("/:id", (req, res) => {
-	const orders = [];
-	for (let i = 0; i < ordersDbData.data.length; i++) {
-		if (ordersDbData.data[i].fUserId === req.params.id || ordersDbData.data[i].eUserId === req.params.id) {
-			orders.push(ordersDbData.data[i]);
-		}
-	}
-	res.json(orders);
+	orderModel.find({$or:[
+		{fUserId: req.params.id},
+		{eUserId: req.params.id},
+		//{_id:new objectId(req.params.id)}
+	]}).then((data) => res.json(data));
 });
 
-router.post("/", (req, res) => {
-	const order = {
-		fUserId: req.body.fUserId,
-		eUserId: req.body.eUserId,
-		productId: req.body.productId,
-		quantity: req.body.quantity,
-		price: req.body.price,
-	};
-	let a;
-	for (let i = 0; i < productDbData.data.length; i++) {
-		if (productDbData.data[i].productId === order.productId) {
-			a = i;
-			break;
-		}
-	}
-	if (productDbData.data[a].quantity < order.quantity) {
-		res.status(400).json({ success: false, message: "Required Quantity is less than Available Quantity" });
+router.post("/", async(req, res) => {
+	
+	let availableQuantity;
+	await productModel.findById(req.body.productId).then((data) => availableQuantity = data.availableQuantity);
+	if (availableQuantity < req.body.quantity) {
+		res.json({
+			success: false,
+			message: "Required quantity is less than available quantity",
+		});
 	} else {
-		productDbData.data[a].quantity -= order.quantity;
-		productDb.write(productDbData);
-		ordersDbData.data.push(order);
-		ordersDb.write(ordersDbData);
+		let order = new orderModel(req.body);
+		order.save();
 		res.status(201).json({ success: true, data: order });
 	}
+	//have to update the available quantity of the product in product model.
+
+
 });
 
 export default router;
