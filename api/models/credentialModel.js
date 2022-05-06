bf2848e6846d03c4e04b8bc7297ab33e16c1f78f
Harshith2001 <harshith1450@gmail.com>
2022-05-06 22:28:32 +0530

    Installed few packages for authentication

---
+++ api/models/credentialModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import connection from "../util/connection.js";
+connection;
+let credentialSchema = new mongoose.Schema({
+	userId: String,
+	password: String,
+});
+
+let credentialModel = mongoose.model("Credential", credentialSchema);
+export default credentialModel;
