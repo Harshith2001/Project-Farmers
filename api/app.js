8a41a2469a2c29ee6d911560370757dafc689473
U-C-S <ucs3721@outlook.com>
2022-06-07 21:10:35 +0530

    remove unused `api` packages

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import helmet from "helmet";
-import session from "express-session";
+// import session from "express-session";
 
 import mainRoute from "./routes/index.js";
 import authRoute from "./routes/auth/auth.js";
@@ -13,15 +13,15 @@ app.use(
 		origin: "*",
 	})
 );
-app.use(session({
-	secret: "superdupersecret",
-	resave: true,
-	saveUninitialized: true,
-	cookie: {
-		domain: "localhost",
-		maxAge: 1000 * 60 * 60 * 24 * 7,
-	},
-}));
+// app.use(session({
+// 	secret: "superdupersecret",
+// 	resave: true,
+// 	saveUninitialized: true,
+// 	cookie: {
+// 		domain: "localhost",
+// 		maxAge: 1000 * 60 * 60 * 24 * 7,
+// 	},
+// }));
 app.use(json());
 app.use(helmet());
 
