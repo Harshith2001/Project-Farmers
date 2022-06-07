14efef89f84f660b42852b5509a9c3bd7262ca71
Harshith2001 <harshith1450@gmail.com>
2022-06-07 22:00:20 +0530

    make connection string as env

--- api/util/connection.js
+++ api/util/connection.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost:27017/Project_Farmers', {useNewUrlParser: true});
-let connection=mongoose.connection;
+import "dotenv/config";
 
-connection.on('connected', function() {
-    console.log('database is connected successfully');
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
+let connection = mongoose.connection;
+
+connection.on("connected", function () {
+	console.log("database is connected successfully");
 });
-connection.on('disconnected',function(){
-    console.log('database is disconnected successfully');
-})
-connection.on('error', console.error.bind(console, 'connection error:'));
+connection.on("disconnected", function () {
+	console.log("database is disconnected successfully");
+});
+connection.on("error", console.error.bind(console, "connection error:"));
 
 export default connection;
