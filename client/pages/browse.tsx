27217b269da2ca21d011705b972d36ff878c74ae
Harshith2001 <harshith1450@gmail.com>
2022-06-09 21:16:38 +0530

    Ui Changes

--- client/pages/browse.tsx
+++ client/pages/browse.tsx
@@ -1,26 +1,36 @@
-import { AppShell, Button, Group, NumberInput, Paper, Title } from "@mantine/core";
-import React, { useEffect } from "react";
+import { AppShell, Button, Grid, Group, Modal, NumberInput, Paper, Title } from "@mantine/core";
+import { Module } from "module";
+import React, { useEffect, useState } from "react";
 import HeaderMenuColored from "../components/NavHeader";
 
 function ProductCard({ data }: { data: ProductInfoDto | null }) {
-  const [Hover, setHover] = React.useState(false);
+  const [opened, setOpened] = useState(false);
 
   return (
-    <Paper shadow="sm" radius="md" p="md" withBorder onMouseOver={() => setHover(true)}>
+    <Paper shadow="sm" radius="md" p="md" withBorder>
       <Group>
         <Title order={2}>{data?.cropName || "Tomato"}</Title>
         <p>{data?.availableQuantity || "100kg"}kg</p>
         <p>by {data?.userId || "godzilla"}</p>
+        <Button onClick={() => setOpened(true)}>Buy</Button>
+        <Modal
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title={`${data?.cropName} by ${data?.userId}`}>
+          <Paper radius="md" p="md" withBorder>
+            <Group>
+              <NumberInput placeholder="Bid value (in string)" label="Bid Value" required />
+              <NumberInput
+                placeholder="(in number)"
+                label="Quantity"
+                required
+                max={data?.availableQuantity}
+              />
+              <Button>Buy</Button>
+            </Group>
+          </Paper>
+        </Modal>
       </Group>
-      {Hover && (
-        <>
-          <Group>
-            <NumberInput placeholder="Bid value (in string)" label="Bid Value" required />
-            <NumberInput placeholder="(in number)" label="Quantity" required />
-          </Group>
-          <Button>Buy</Button>
-        </>
-      )}
     </Paper>
   );
 }
@@ -35,11 +45,13 @@ export default function browse() {
 
   return (
     <AppShell padding={0} header={<HeaderMenuColored />}>
-      <Group>
+      <Grid p={10}>
         {data.map((item) => (
-          <ProductCard data={item} />
+          <Grid.Col span={4}>
+            <ProductCard data={item} />
+          </Grid.Col>
         ))}
-      </Group>
+      </Grid>
     </AppShell>
   );
 }
