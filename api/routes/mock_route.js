a126de03377730e208ea1694bb6b85a1b141ab29
Harshith2001 <harshith1450@gmail.com>
2022-07-01 18:58:42 +0530

    .

--- api/routes/mock_route.js
+++ api/routes/mock_route.js
@@ -23,8 +23,9 @@ router.post("/ship/", (req, res) => {
   ship.save().then((data) => res.json(data));
 });
 
-router
-  .get("/orders", (req, res) => {
+router.get("/orders", (req, res) => {
+  // As mongo Atlas free tire doesn't support $cond operator, I am using if-else statement.
+  if (req.query.city !== undefined) {
     customerModel
       .aggregate([
         {
@@ -50,16 +51,94 @@ router
             email: 1,
             phone: 1,
             city: 1,
-            purchases: {
-              productName: "$purchases.productName",
-              quantity: "$purchases.quantity",
-              pricing: "$purchases.pricing",
-              mrp: "$purchases.mrp",
-            },
+            purchases: "$purchases",
           },
         },
       ])
       .then((data) => res.json(data));
-  });
+  } else {
+    console.log("No query");
+    customerModel
+      .aggregate([
+        { $addFields: { customerId: { $toString: "$_id" } } },
+        {
+          $lookup: {
+            from: "purchases",
+            localField: "customerId",
+            foreignField: "customerId",
+            as: "purchases",
+          },
+        },
+        { $unwind: "$purchases" },
+        {
+          $project: {
+            _id: 1,
+            customerId: 1,
+            customerName: 1,
+            email: 1,
+            phone: 1,
+            city: 1,
+            purchases: "$purchases",
+          },
+        },
+      ])
+      .then((data) => res.json(data));
+  }
+});
+
+router.get("/shipping", (req, res) => {
+  shipModel
+    .aggregate([
+      {
+        $addFields: {
+          purchaseId: { $toObjectId: "$purchaseId" },
+          customerId: { $toObjectId: "$customerId" },
+        },
+      },
+      {
+        $lookup: {
+          from: "purchases",
+          localField: "purchaseId",
+          foreignField: "_id",
+          as: "purchases",
+        },
+      },
+      { $unwind: "$purchases" },
+      {
+        $lookup: {
+          from: "customers",
+          localField: "customerId",
+          foreignField: "_id",
+          as: "customers",
+        },
+      },
+      { $unwind: "$customers" },
+      {
+        $project: {
+          customer: {
+            customerName: "$customers.customerName",
+            email: "$customers.email",
+            phone: "$customers.phone",
+            city: "$customers.city",
+            purchaseOrder:{
+              purchaseId: "$purchases._id",
+              customerId: "$customers._id",
+              productName: "$purchases.productName",
+              pricing: "$purchases.pricing",
+              mrp: "$purchases.mrp",
+              quantity: "$purchases.quantity",
+              shipmentDetails:{
+                shipmentId: "$_id",
+                address: "$address",
+                city: "$city",
+                state: "$state",
+              }
+            }
+          }
+        },
+      },
+    ])
+    .then((data) => res.json(data));
+});
 
 export default router;
