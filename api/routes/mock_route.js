84dfce9d043810a45f517de0fae16b4f32853f3e
Harshith2001 <harshith1450@gmail.com>
2022-07-26 17:24:22 +0530

    added multer package

--- api/routes/mock_route.js
+++ api/routes/mock_route.js
@@ -6,18 +6,6 @@ router.get("/:id", (req, res) => {
   demandModel.findOne({ cropName: req.params.id }).then((data) => res.json(data));
 });
 
-router.post("/", (req, res) => {
-  let demandData = new demandModel(req.body);
-  demandData.save((err, data) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.status(200).json({
-        message: "Success",
-        data: data,
-      });
-    }
-  });
-});
+router.post("/", (req, res) => {});
 
 export default router;
